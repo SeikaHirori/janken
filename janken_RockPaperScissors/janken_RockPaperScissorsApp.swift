3bdded9215c633ff810af75bcc8b1d2002a066c0
SeikaHirori <SeikaHirori@outlook.com>
2023-02-16 10:40:59 -0800

    Initial Commit

---
+++ janken_RockPaperScissors/janken_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  janken_RockPaperScissorsApp.swift
+//  janken_RockPaperScissors
+//
+//  Created by Seika Hirori on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct janken_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
