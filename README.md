d21507b7fc754703e5983642eee80e5e1dadafee
SeikaHirori <SeikaHirori@outlook.com>
2023-02-16 19:09:08 -0800

    Added multiple features: - fixed var "currentLanguage"'s Japanese 「チョキ」to remove a space - added var "switchToLang" - Added note to bool "shouldPlayerWin" - added var "score_player_victories_count" for keeping track of when player beats bot - added var "previousScore" for debugging; meant to see what the final score from last round was - added Text to display "previousScore" and "score_player_victories_count" - added button to switch between English and Japanese - Implemented property that works for outputs buttons of RPS - modified switchLang()  to included updating var "switchToLang" and "currentLang" - added modifier SettingsStyle()

--- README.md
+++ README.md
@@ -25,9 +25,9 @@ ___
 
 - [x] 1. Start with an App template, then create a property to store the three possible moves: rock, paper, and scissors.
 - [x] 2. You’ll need to create two @State properties to store the app’s current choice and whether the player should win or lose.
-- [ ] 3. You can use Int.random(in:) to select a random move. You can use it for whether the player should win too if you want, but there’s an even easier choice: Bool.random() is randomly true or false. After the initial value, use toggle() between rounds so it’s always changing.
+- [x] 3. You can use Int.random(in:) to select a random move. You can use it for whether the player should win too if you want, but there’s an even easier choice: Bool.random() is randomly true or false. After the initial value, use toggle() between rounds so it’s always changing.
 - [ ] 4. Create a VStack showing the player’s score, the app’s move, and whether the player should win or lose. You can use if shouldWin to return one of two different text views.
-- [ ] 5. The important part is making three buttons that respond to the player’s move: Rock, Paper, or Scissors.
+- [x] 5. The important part is making three buttons that respond to the player’s move: Rock, Paper, or Scissors.
 - [ ] 6. Use the font() modifier to adjust the size of your text. If you’re using emoji for the three moves, they also scale. Tip: You can ask for very large system fonts using .font(.system(size: 200)) – they’ll be a fixed size, but at least you can make sure they are nice and big!
 
 ___
--- janken_RockPaperScissors/ContentView.swift
+++ janken_RockPaperScissors/ContentView.swift
@@ -8,56 +8,133 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var currentLanguage:[String] = ["グー (guu)","パー (paa)" ,"チョキ (choki)"] // Default to Japanese on launch
     
-    @State private var currentLanguage:[String] = ["グー (guu)","パー (paa)" ,"チョキ　(choki)"] // Default to Japanese on launch
+    @State private var switchToLang: String = "Switch to:\nEnglish"
     @State private var useEnglish:Bool = false
     
     @State private var app_RPS_choice: Int = Int.random(in: 0...2) // CS #2
-    @State private var shouldPlayerWin: Bool = Bool.random() // CS #2
+    @State private var shouldPlayerWin: Bool = Bool.random() // CS #2 & CS #3
     
     @State private var currentRound: Int = 1
+    
+    @State private var score_player_victories_count:Int = 0
+    @State private var previousScore:String = "" // Track score for debugging purposes
     
     var body: some View {
         VStack {
             Text("Round: \(currentRound)")
                 .roundTitleCount()
+            Text(previousScore)
+            Text("Score: \(score_player_victories_count)")
+
             listRPS
+            
+            Button("\(switchToLang)") {
+                useEnglish.toggle()
+                switchLanguage()
+            }
+            .settings_style()
+            
+            
+            
+            
+            Text("debug | bot chose: \(app_RPS_choice)")
+            
+            
         }
     }
     
     // CS #1
     var listRPS: some View {
         VStack {
-            ForEach(rockPaperScissors.allCases, id: \.self) {
-                let itemValue:String = "\(currentLanguage[$0.rawValue])"
-                let rpsValue:Int = $0.rawValue
+            ForEach(rockPaperScissors.allCases, id: \.self) { optionRPS in
+                let itemValue:String = "\(currentLanguage[optionRPS.rawValue])"
+                let currentChoice: rockPaperScissors = optionRPS
 //                Text("DEBUG #\($0.rawValue): \(itemValue)")
 //                    .padding()
                 
                 Button {
-                    playerSelectChoice_fightTime(rpsValue)
+                    playerSelectChoice_fightTime(currentChoice)
                 } label: {
                     Text("\(itemValue)")
                 }
                 .buttonRPS_style()
-                
             }
         }
     }
     
     func switchLanguage() -> () {
         let englishRPS:[String] = ["Rock", "Paper", "Scissor"]
-        let japaneseRPS:[String] = ["グー (guu)","パー (paa)" ,"チョキ　(choki)"]
+        let japaneseRPS:[String] = ["グー (guu)","パー (paa)" ,"チョキ (choki)"]
         
-        if useEnglish {
-            currentLanguage = englishRPS
-        } else {
-            currentLanguage = japaneseRPS
-        }
+        let template_switchTo:String = "Switch to:\n"
+        let string_switchToEnglish:String = template_switchTo + "English"
+        let string_switchToJapanese:String = template_switchTo + "Japanese"
+        
+        
+        
+        switchToLang = useEnglish ? string_switchToJapanese : string_switchToEnglish
+        currentLanguage = useEnglish ? englishRPS : japaneseRPS
+//
+//        if useEnglish {
+//            currentLanguage = englishRPS
+//        } else {
+//            currentLanguage = japaneseRPS
+//        }
     }
     
-    func playerSelectChoice_fightTime(_ choice:Int) {
-        print("Choice #\(choice): \(currentLanguage[choice])")
+    func playerSelectChoice_fightTime(_ choice:rockPaperScissors) {
+        print("Choice #\(choice): \(currentLanguage[choice.rawValue])")
+        
+        let rpsRawValue = choice.rawValue
+        
+        // Handle draw here
+        if rpsRawValue == app_RPS_choice {
+            
+        }
+        
+        switch rpsRawValue {
+        // Rock
+        case 0:
+            if app_RPS_choice == 2 {
+                victoryMeansPlusOneToScore()
+            }
+        // Paper
+        case 1:
+            if app_RPS_choice == 0 {
+                victoryMeansPlusOneToScore()
+            }
+        // Scissors
+        case 2:
+            if app_RPS_choice == 1 {
+                victoryMeansPlusOneToScore()
+            }
+            
+        default:
+            break
+        }
+        
+        nextRound()
+    }
+    
+    func victoryMeansPlusOneToScore() {
+        score_player_victories_count += 1
+    }
+    
+    func nextRound() {
+        if currentRound == 10 {
+            print("You finished all 10 rounds :3")
+            print("Your score: \(score_player_victories_count)")
+            previousScore = "Previous score: \(score_player_victories_count)"
+            currentRound = 0
+            score_player_victories_count = 0
+        }
+        
+        app_RPS_choice = Int.random(in: 0...2) // CS #3
+        
+        currentRound += 1
     }
 }
 
@@ -77,6 +154,10 @@ extension View {
     func buttonRPS_style() -> some View {
         modifier(ButtonRPSstyle())
     }
+    
+    func settings_style() -> some View {
+        modifier(SettingsStyle())
+    }
 }
 
 struct RoundTitleCount: ViewModifier {
@@ -95,8 +176,20 @@ struct ButtonRPSstyle: ViewModifier {
     func body(content: Content) -> some View {
         content
             .padding(20)
-            .frame(maxWidth: 150, maxHeight: 75)
+            .frame(maxWidth: 200, maxHeight: 75)
             .background(Color.blue)
+            .foregroundColor(Color.white)
+            .font(.system(size: 25))
+            .clipShape(Capsule())
+    }
+}
+
+struct SettingsStyle:ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(5)
+            .frame(maxWidth: 100)
+            .background(Color.red)
             .foregroundColor(Color.white)
             .clipShape(Capsule())
     }
